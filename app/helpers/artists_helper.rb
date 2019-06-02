246891da9398419de4afebe1e2d9ba25e65401ea
Joy Lin <vvlnote@hotmail.com>
2019-06-02 02:27:38 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+  def display_artist(song)
+    if !song.nil? && !song.artist.nil?
+      aritst.name
+      redirect_to artist_path(song.artist)
+    else
+      "Add Artist"
+      redirect_to edit_song_path(song)
+    end
+  end
+
 end
