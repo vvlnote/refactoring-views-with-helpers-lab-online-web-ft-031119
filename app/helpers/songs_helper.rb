c5dc7429aab37c3aafecc69abf35279772c698dd
Joy Lin <vvlnote@hotmail.com>
2019-06-02 01:57:34 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+  def display_artist(song)
+    if !song.nil? && !song.artist.nil?
+      true
+    else
+      false
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    artist = Artist.find_by(:name => name)
+    if artist.nil?
+      artist = Artist.create(:name=>name)
+    end
+    self.artist = artist
   end
 end
